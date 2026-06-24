0ca7d553f1955976394f18dc6f5a819d90e7c2eb
Hieu Phan Trung <phantrunghieu1291@gmail.com>
2026-06-24 15:37:24 +0700

    UPDATE: Create front page template

---
+++ front-page-template.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * @author Hieu "Jin" Phan Trung
+ * * Template name: JINS TEMPLATE - Front page template
+ */
+get_template_part( 'gpw-templates/global/header' );
+get_template_part( 'gpw-templates/global/footer' );

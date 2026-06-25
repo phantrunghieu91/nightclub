439c34769e9e3b1ce59f4205ecc22992f6c19000
Hieu Phan Trung <phantrunghieu1291@gmail.com>
2026-06-25 16:12:20 +0700

    UPDATE: Hero section template

---
+++ gpw-templates/global/hero-section.php
@@ -0,0 +1,14 @@
+<?php 
+/**
+ * @author Hieu "Jin" Phan Trung
+ * * Template: Global - Hero section
+ */
+$sectionData = get_field( 'hero' );
+if( empty( $sectionData['banner'] )) {
+  return;
+} ?>
+<section class="hero">
+  <div class="section__inner section__inner--full">
+    <?= wp_get_attachment_image( $sectionData['banner'], 'full', false, ['class' => 'hero__banner', 'alt' => 'Hero banner']) ?>
+  </div>
+</section>

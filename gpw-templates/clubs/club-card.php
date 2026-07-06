ce178d4a5bb3a3d0ea9c8cda13de943f5a90ccd0
Hieu Phan Trung <phantrunghieu1291@gmail.com>
2026-07-06 14:01:10 +0700

    UPDATE: club card - Handle orientation and excerpt display conditional

--- gpw-templates/clubs/club-card.php
+++ gpw-templates/clubs/club-card.php
@@ -1,21 +1,29 @@
-<?php 
+<?php
 /**
  * @author Hieu "Jin" Phan Trung
  * * Template: Club card
  */
-$title = get_the_title();
-$excerpt = get_the_excerpt();
-$link = get_permalink();
+$showExcerpt = $args['show_excerpt'] ?? true;
+$orientation = $args['orientation'] ?? 'horizontal';
+$title       = get_the_title();
+$excerpt     = get_the_excerpt();
+$link        = get_permalink();
 $thumbnailID = get_post_thumbnail_id() ?? PLACEHOLDER_IMAGE_ID;
+$classes = ['club-card', 'jins-card'];
+if( $orientation === 'horizontal' ) {
+  $classes = array_merge([ 'jins-card--horizontal', 'jins-card--padded' ], $classes);
+}
 ?>
-<article class="jins-card jins-card--horizontal jins-card--padded">
+<article class="<?= esc_attr( implode( ' ', $classes )) ?>">
   <a href="<?= esc_url( $link ) ?>" class="jins-card__thumbnail">
-    <?= wp_get_attachment_image( $thumbnailID, 'medium_large', false, [ 'alt' => $title ]) ?>
+    <?= wp_get_attachment_image( $thumbnailID, 'medium_large', false, [ 'alt' => $title ] ) ?>
   </a>
   <div class="jins-card__content">
     <h2 class="jins-card__title">
       <a href="<?= esc_url( $link ) ?>"><?= esc_html( $title ) ?></a>
     </h2>
-    <div class="jins-card__excerpt"><?= wp_kses_post( $excerpt ) ?></div>
+    <?php if( $showExcerpt && !empty( $excerpt ) ) : ?>
+      <div class="jins-card__excerpt"><?= wp_kses_post( $excerpt ) ?></div>
+    <?php endif ?>
   </div>
 </article>

79aa36f0d2f95b4ed64ef8928c883fc1ba98b0a7
Hieu Phan Trung <phantrunghieu1291@gmail.com>
2026-07-13 15:25:04 +0700

    UPDATE: Archive clubs page - Added filter club block

--- archive-clubs.php
+++ archive-clubs.php
@@ -8,6 +8,8 @@ get_template_part( 'gpw-templates/global/header' );
 
 get_template_part( 'gpw-templates/clubs/category/header' );
 
+get_template_part( 'gpw-templates/clubs/category/clubs-filter' );
+
 get_template_part( 'gpw-templates/clubs/category/content' );
 
 get_template_part( 'gpw-templates/global/footer' );

1575aa0f53a44c5a4e894b9fda59924cd02cd0fe
Hieu Phan Trung <phantrunghieu1291@gmail.com>
2026-07-13 15:23:36 +0700

    UPDATE: Club card - Added render template option

--- gpw-templates/clubs/club-card.php
+++ gpw-templates/clubs/club-card.php
@@ -3,27 +3,42 @@
  * @author Hieu "Jin" Phan Trung
  * * Template: Club card
  */
+$isTemplate  = $args['is_template']  ?? false;
 $showExcerpt = $args['show_excerpt'] ?? true;
-$orientation = $args['orientation'] ?? 'horizontal';
+$orientation = $args['orientation']  ?? 'horizontal';
 $title       = get_the_title();
 $excerpt     = get_the_excerpt();
 $link        = get_permalink();
 $thumbnailID = get_post_thumbnail_id() ?? PLACEHOLDER_IMAGE_ID;
-$classes = ['club-card', 'jins-card'];
+$classes     = ['club-card', 'jins-card'];
 if( $orientation === 'horizontal' ) {
-  $classes = array_merge([ 'jins-card--horizontal', 'jins-card--padded' ], $classes);
+  $classes = array_merge( [ 'jins-card--horizontal', 'jins-card--padded' ], $classes );
 }
 ?>
-<article class="<?= esc_attr( implode( ' ', $classes )) ?>">
-  <a href="<?= esc_url( $link ) ?>" class="jins-card__thumbnail">
-    <?= wp_get_attachment_image( $thumbnailID, 'medium_large', false, [ 'alt' => $title ] ) ?>
+<?php if( true === $isTemplate ) {
+  echo '<template id="club-card-template">';
+} ?>
+<article 
+  <?php if ( $isTemplate ) {
+    echo sprintf( 'class="%s"', implode( ' ', $classes ) );
+  } else post_class( $classes ); ?> 
+>
+  <a href="<?= !$isTemplate ? esc_url( $link ) : '' ?>" class="jins-card__thumbnail">
+    <?php if( false === $isTemplate ) {
+      echo wp_get_attachment_image( $thumbnailID, 'medium_large', false, [ 'alt' => $title ] );
+    } else {
+      echo '<img src="" srcset="" alt="Image alt" />';
+    } ?>
   </a>
   <div class="jins-card__content">
     <h2 class="jins-card__title">
-      <a href="<?= esc_url( $link ) ?>"><?= esc_html( $title ) ?></a>
+      <a href="<?= !$isTemplate ? esc_url( $link ) : '' ?>"><?= !$isTemplate ? esc_html( $title ) : '' ?></a>
     </h2>
     <?php if( $showExcerpt && !empty( $excerpt ) ) : ?>
-      <div class="jins-card__excerpt"><?= wp_kses_post( $excerpt ) ?></div>
+      <div class="jins-card__excerpt"><?= !$isTemplate ? wp_kses_post( $excerpt ) : '' ?></div>
     <?php endif ?>
   </div>
 </article>
+<?php if( true === $isTemplate ) {
+  echo '</template>';
+}
